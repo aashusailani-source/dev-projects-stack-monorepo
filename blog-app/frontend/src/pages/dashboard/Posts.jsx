c12cc047458685170413a66f83bde4952dc3c1a0
Aashu Sailani <rahulsailani619@gmail.com>
2025-01-18 04:43:19 +0530

    frontend changes v3

--- blog-app/frontend/src/pages/dashboard/Posts.jsx
+++ blog-app/frontend/src/pages/dashboard/Posts.jsx
@@ -47,14 +47,14 @@ function Posts() {
             onClick={() => navigate("/dashboard/posts/create")}
             className="mt-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-6 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-500 transition duration-300"
           >
-            {posts.length > 0 ? "Login or SignUp to Create Another Post" : "Login or SignUp to create a post"}
+            {posts.length > 0 ? "Create Another Post" : "Create your first post"}
           </button>
         ) : (
           <span
             // onClick={() => navigate("/dashboard/posts/create")}
             className="mt-4 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-6 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-500 transition duration-300"
           >
-            {posts.length > 0 ? "Create Another Post" : "Create Your First Post"}
+            {posts.length > 0 ? "Login or SignUp to create another post" : "Login or SignUp Create Your First Post"}
           </span>
         )}
       </div>
