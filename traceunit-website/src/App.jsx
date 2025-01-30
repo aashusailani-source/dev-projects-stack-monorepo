36f00071b09a0200aa1bd5962ef4f5dea7622f96
Aashu Sailani <rahulsailani619@gmail.com>
2025-01-30 09:37:09 +0530

    bot website

---
+++ traceunit-website/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center flex-col gap-5 bg-black text-white min-h-screen">
+      <h1 className="font-bold text-4xl">Welcome to twitter bot</h1>
+      <p>This is a bot, I&apos;ve made just for fun</p>
+    </div>
+  )
+}
